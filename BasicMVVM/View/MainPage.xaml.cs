04ecd7e0b26974d3e1aac76ac0b96af880a6375f
Guy Cizner <ciznerguy@hotmail.com>
2024-01-09 21:21:06 +0200

    Add project files.

---
+++ BasicMVVM/View/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using BasicMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BasicMVVM.View
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Window
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DataContext = new UserViewModel();
+            Menu mainMenu = (Menu)FindResource("MainMenu");
+
+        }
+        private void Exit_Clicked(object sender, RoutedEventArgs e)
+        {
+            // Global exit logic here
+            Close();
+        }
+
+
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+        
+            Close();
+        }
+
+    }
+}
---
+++ BasicMVVM/ViewModel/UserViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using BasicMVVM.Model;
+
+namespace BasicMVVM.ViewModel
+{
+    class UserViewModel
+    {
+        private List<User> _UsersList;
+
+        public UserViewModel()
+        {
+            _UsersList = new List<User>
+            {
+               new User { UserId = 1, FirstName = "אביגיל", LastName = "כהן", City = "תל אביב", State = "TA", Country = "ישראל", Email = "avigail@example.com" },
+new User { UserId = 2, FirstName = "משה", LastName = "לוי", City = "ירושלים", State = "JM", Country = "ישראל", Email = "moshe@example.com" },
+new User { UserId = 3, FirstName = "שרה", LastName = "גולן", City = "חיפה", State = "HA", Country = "ישראל", Email = "sarah@example.com" },
+new User { UserId = 4, FirstName = "יעקב", LastName = "פרץ", City = "באר שבע", State = "BS", Country = "ישראל", Email = "yaakov@example.com" },
+new User { UserId = 5, FirstName = "רחל", LastName = "מאיר", City = "אשדוד", State = "ASD", Country = "ישראל", Email = "rachel@example.com" },
+new User { UserId = 6, FirstName = "דוד", LastName = "גור", City = "נתניה", State = "NT", Country = "ישראל", Email = "david@example.com" },
+new User { UserId = 7, FirstName = "מרים", LastName = "זכאי", City = "עפולה", State = "AF", Country = "ישראל", Email = "miriam@example.com" },
+new User { UserId = 8, FirstName = "עידו", LastName = "אביטל", City = "אילת", State = "IL", Country = "ישראל", Email = "ido@example.com" },
+new User { UserId = 9, FirstName = "תמר", LastName = "פלאי", City = "טבריה", State = "TB", Country = "ישראל", Email = "tamar@example.com" },
+new User { UserId = 10, FirstName = "יפה", LastName = "כהן", City = "קריית שמונה", State = "KS", Country = "ישראל", Email = "yafe@example.com" },
+
+            };
+        }
+
+        public List<User> Users
+        {
+            get { return _UsersList; }
+            set { _UsersList = value; }
+        }
+        
+    }
+}
